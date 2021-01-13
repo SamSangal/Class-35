fb9869e9049dd4169ec4f659ac86310dc4de875f
Samarthya Sangal <samarthya.sangal.10@gmail.com>
2021-01-12 20:57:03 -0600

    added Data Base

--- sketch.js
+++ sketch.js
@@ -1,29 +1,54 @@
 var ball;
-
+var database, position;
+function preload(){
+    car=loadImage("download.png")
+}
 function setup(){
+    database=firebase.database();
     createCanvas(500,500);
+
     ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    ball.addImage(car);
+    var ballPosition=database.ref("ball/position");
+    ballPosition.on("value",readPosition,showErr);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
+        ball.rotation=180;
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
+        ball.rotation=0;
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        ball.rotation=270;
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
+        ball.rotation=90;
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        x:position.x+x,
+        y:position.y+y
+    })
+    
+}
+
+function readPosition(data){
+position=data.val();
+console.log(position.x);
+ball.x=position.x;
+ball.y=position.y;
+}
+
+function showErr(){
+    console.log("Error");
 }
